04498430483abdb2c1c7f46a46287da535dde2a2
Kim inha <12200403@inha.edu>
2024-08-05 15:57:09 +0900

    알고리즘 수행 시간1

--- baekjoon.py
+++ baekjoon.py
@@ -1,1 +1,8 @@
 # 시간복잡도 알고리즘 수행 시간1 : MenOfPassion 알고리즘 수행 횟수 출력, 다음 줄에 수행 횟수를 다항식으로 나타내었을 때, 최고차항의 차수 출력
+def MenOfPassion(A, n):
+    i = n//2
+    return A[i]
+
+n = int(input())
+print(1)
+print(0)
