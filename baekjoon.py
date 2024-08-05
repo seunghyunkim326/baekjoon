29b14a0ed5e18600b0cb656e0a82d0b4d48ace38
Kim inha <12200403@inha.edu>
2024-08-05 16:06:15 +0900

    알고리즘 수행 시간2 with 풀이

--- baekjoon.py
+++ baekjoon.py
@@ -1,8 +1,11 @@
-# 시간복잡도 알고리즘 수행 시간1 : MenOfPassion 알고리즘 수행 횟수 출력, 다음 줄에 수행 횟수를 다항식으로 나타내었을 때, 최고차항의 차수 출력
-def MenOfPassion(A, n):
-    i = n//2
-    return A[i]
+# 시간복잡도 알고리즘 수행 시간2 : 수행 횟수, 다음 줄에 수행 횟수를 다항식으로 표현 시, 최고차항의 차수 출력
+def MenOfPassion(A,n):
+    sum = 0
+    for i in range(1,n):
+        sum = sum + A[i]
+    return sum
+# n만큼 실행됨 => A배열의 길이가 n보다 크거나 같다는 가정하에 => O(n)의 시간복잡도를 가짐
 
 n = int(input())
+print(n)
 print(1)
-print(0)
