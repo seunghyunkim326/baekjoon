0994801bec2a6b8466f33f7e28ef62dc6522f904
Kim inha <12200403@inha.edu>
2024-08-05 17:18:06 +0900

    브루트 포스 블랙잭

--- baekjoon.py
+++ baekjoon.py
@@ -1,11 +1,16 @@
-# 시간복잡도 알고리즘 수행 시간2 : 수행 횟수, 다음 줄에 수행 횟수를 다항식으로 표현 시, 최고차항의 차수 출력
-def MenOfPassion(A,n):
-    sum = 0
-    for i in range(1,n):
-        sum = sum + A[i]
-    return sum
-# n만큼 실행됨 => A배열의 길이가 n보다 크거나 같다는 가정하에 => O(n)의 시간복잡도를 가짐
-
-n = int(input())
-print(n)
-print(1)
+# 브루트 포스 블랙잭 : 카드 개수 N, 고른 카드의 합 M을 입력받음, 다음 줄에 N개의 카드를 입력받음
+# 3장의 카드의 합이 M을 넘지 않고 M에 가장 가까운 값(3자의 카드 합) 출력
+N, M =map(int,input().split())
+arr = [int(x) for x in input().split()]
+sum = 0
+for i in range(N):
+    for j in range(i+1,N):
+        for k in range(j+1,N):
+            temp = arr[i] + arr[j] + arr[k]
+            if temp > M:
+                pass
+            else:
+                if temp > sum:
+                    sum = temp
+print(sum)
+# 
