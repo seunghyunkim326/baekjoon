98ade4d820912d949a83557eda4b770ef019e199
Kim inha <12200403@inha.edu>
2024-08-07 19:38:14 +0900

    알고리즘 수행 시간5(너무 쉬움)

--- baekjoon.py
+++ baekjoon.py
@@ -1,10 +1,12 @@
-# 알고리즘 수행 시간4
+# 알고리즘 수행 시간5
 def MenOfPassion(A,n):
     sum = 0
-    for i in range(1,n):
-        for j in range(i+1,n+1):
-            sum = sum + A[i] * A[j]
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            for k in range(1,n+1):
+                sum = sum + A[i] * A[j] * A[k]
     return sum
+
 N = int(input())
-print(int((N/2)*(N-1)))
-print(2)
+print(N**3)
+print(3)
