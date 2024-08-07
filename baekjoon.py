194deeac9c27589fe147f499d5f3f3ecc45ba1f9
Kim inha <12200403@inha.edu>
2024-08-07 17:52:40 +0900

    알고리즘 수행 시간4(1~n까지의 합 식을 몰랐네ㅜㅜ)

--- baekjoon.py
+++ baekjoon.py
@@ -1,10 +1,10 @@
-# 알고리즘 수행 시간3
+# 알고리즘 수행 시간4
 def MenOfPassion(A,n):
     sum = 0
-    for i in range(1,n+1):
-        for j in range(1,n+1):
+    for i in range(1,n):
+        for j in range(i+1,n+1):
             sum = sum + A[i] * A[j]
     return sum
 N = int(input())
-print(N**2)
+print(int((N/2)*(N-1)))
 print(2)
