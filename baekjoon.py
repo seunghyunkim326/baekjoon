e7a5d64da9744924b7dfd850cf06c31a297854f3
Kim inha <12200403@inha.edu>
2024-08-09 18:47:24 +0900

    수 정렬하기(쉬움)

--- baekjoon.py
+++ baekjoon.py
@@ -1,13 +1,21 @@
-# 알고리즘 수행 시간6
-def MenOfPassion(A,n):
-    sum = 0
-    for i in range(1,n-1):
-        for j in range(i+1,n):
-            for k in range(j+1,n+1):
-                sum = sum + A[i] * A[j] * A[k]
-    return sum
-
-# (n-2)
+# 수 정렬하기 : N개의 수 입력 => 이를 오름차순으로 출력(중복X)
+# 첫째 줄에 수의 개수 N입력. 둘째 줄부터 N개의 줄에 수가 주어짐.
 N = int(input())
-print(int((N*(N-1)*(N-2))/6))
-print(3)
+arr = []
+for _ in range(N):
+    num = int(input())
+    arr.append(num)
+# 오름차순으로 정렬하는 코드
+for i in range(N):
+    for j in range(i,N):
+        if arr[i] > arr[j]:
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+# 중복없는 오름차순 리스트를 만드는 코드(중복이 아닐 경우에 넣는 코드)
+result = []
+for k in range(N):
+    if arr[k] not in result:
+        result.append(arr[k])
+for r in range(len(result)):
+    print(result[r])
