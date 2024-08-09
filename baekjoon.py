2a3944e46be8c3ed75ede19cbdedbddb45b10e3c
Kim inha <12200403@inha.edu>
2024-08-09 16:14:14 +0900

    알고리즘 수행 시간6

--- baekjoon.py
+++ baekjoon.py
@@ -1,7 +1,13 @@
-# 수학은 비대면강의 : ax + by =c, dx + ey = f 연립 방정식의 해를 구하는 문제
-a, b, c, d, e, f = map(int,input().split())
-for x in range(-999,999):
-    for y in range(-999,999):
-        if a * x + b * y == c and d * x + e * y == f:
-            print(x,end=' ')
-            print(y)
+# 알고리즘 수행 시간6
+def MenOfPassion(A,n):
+    sum = 0
+    for i in range(1,n-1):
+        for j in range(i+1,n):
+            for k in range(j+1,n+1):
+                sum = sum + A[i] * A[j] * A[k]
+    return sum
+
+# (n-2)
+N = int(input())
+print(int((N*(N-1)*(N-2))/6))
+print(3)
