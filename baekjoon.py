81e384bba729597eee8de7a60c64968b83481937
Kim inha <12200403@inha.edu>
2024-08-10 14:50:38 +0900

    숫자 카드

--- baekjoon.py
+++ baekjoon.py
@@ -1,15 +1,11 @@
-# 알고리즘 수업 - 점근적 표기 1 : f(n) = a1n + a0, 양의 정수 c, n0 입력 => 만족 = 1, 불만족 = 0 출력
-# O(n)의 정의 : 모든 n>=n0 에 대해 f(n)<=c*g(n)인 양의 상수 c와 n0가 존재
-a1, a0 = map(int,input().split())
-c = int(input())
-n0 = int(input())
-n = n0
-while True:
-    if a1 * n + a0 > c * n:
-        print(0)
-        break
-    elif n == 101:
-        print(1)
-        break
+# 숫자 카드 : N, N개의 정수, M, M개의 정수 입력 => 가지고 있으면 1, 없으면 0 출력
+# 가지고 있는 숫자 카드는 N개, N밑은 숫자들을 가짐 => 확인용 M개의 숫자 카드, M밑은 확인용 숫자들
+N = int(input())
+having = [x for x in map(int,input().split())]
+M = int(input())
+check = [y for y in map(int,input().split())]
+for k in range(M):
+    if check[k] in having:
+        print(1,end=' ')
     else:
-        n += 1
+        print(0,end=' ')
