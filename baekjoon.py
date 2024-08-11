006aa315060f7b6d4178b900038555dd17d155da
Kim inha <12200403@inha.edu>
2024-08-11 22:40:50 +0900

    문자열 집합

--- baekjoon.py
+++ baekjoon.py
@@ -1,24 +1,17 @@
-# 숫자 카드 : N, N개의 정수, M, M개의 정수 입력 => 가지고 있으면 1, 없으면 0 출력
-# 가지고 있는 숫자 카드는 N개, N밑은 숫자들을 가짐 => 확인용 M개의 숫자 카드, M밑은 확인용 숫자들
-# N = int(input())
-# having = [x for x in map(int,input().split())]
-# M = int(input())
-# check = [y for y in map(int,input().split())]
-# for k in range(M):
-#     if check[k] in having:
-#         print(1,end=' ')
-#     else:
-#         print(0,end=' ')
-# 시간초과 발생 => 이유가 뭘까? => N, M이 1~500000 사이의 값이라 그런가? => 역시 in 연산자 때문이었다.
-# in 연산자를 사용해서 having 리스트 길이만큼 탐색을 해야하므로 시간 복잡도 O(N*M)을 가진다.
-# 해싱을 이용한 빠른 검색이 가능한 set을 이용하면 해결될것 같다!!
-N = int(input())
-having = {x for x in map(int,input().split())}
-M = int(input())
-check = [y for y in map(int,input().split())]
-for k in range(M):
-    if check[k] in having:
-        print(1,end=' ')
+# 문자열 집합 : N, M 입력, 집합 S에 포함된 N개의 문자열, 검사해야할 M개의 문자열 입력 => M개의 문자열 중 집합 S에 포함된 문자열 갯수 출력
+N, M = map(int,input().split())
+S = set()
+check = []
+count = 0
+for _ in range(N):
+    temp_S = str(input())
+    S.add(temp_S)
+for _ in range(M):
+    temp_check = str(input())
+    check.append(temp_check)
+for i in range(M):
+    if check[i] in S:
+        count += 1
     else:
-        print(0,end=' ')
-# 이러면 O(M)의 시간복잡도를 가지겠지?
+        pass
+print(count)
