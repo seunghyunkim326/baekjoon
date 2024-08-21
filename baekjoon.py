9452bb2d9da02f8c5c8451f65fd4b4b47b7ca48f
Kim inha <12200403@inha.edu>
2024-08-21 17:00:51 +0900

    커트라인(반복문)

--- baekjoon.py
+++ baekjoon.py
@@ -2,6 +2,10 @@
 N, k = map(int,input().split())
 score = [int(x) for x in map(int,input().split())]
 # 내림차순 정렬 함수 or 알고리즘 사용
-score.sort(reverse=True)
-print(score)
+for i in range(N):
+    for j in range(i+1,N):
+        if score[j] > score[i]:
+            temp = score[j]
+            score[j] = score[i]
+            score[i] = temp
 print(score[k-1])
