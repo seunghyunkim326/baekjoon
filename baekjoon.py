9a29d45778bd8d29581ec4709d5c2f00afe89a39
Kim inha <12200403@inha.edu>
2024-08-30 19:32:11 +0900

    나는야 포켓몬 마스터 이다솜(해결!)

--- baekjoon.py
+++ baekjoon.py
@@ -3,22 +3,22 @@
 # M개의 문제에 대한 답을 출력 => 이름이면 도감 번호, 도감 번호면 이름
 import sys
 input = sys.stdin.readline
-output = sys.stdout.write
-N, M = map(int, input().split())
-arr = []
-for i in range(N):
+N, M = map(int, input().strip().split())
+num = 1
+arr = {}
+for _ in range(N):
     name = input()
-    arr.append(name)
+    arr[num] = name
+    num += 1
+reverse_arr = {v:k for k, v in arr.items()}
 for _ in range(M):
     problem = input()
     try:
         problem = int(problem)
+        print(arr[problem])
     except:
         problem = str(problem)
-    if type(problem) == int:
-        output(arr[problem] + "\n")
-    elif type(problem) == str:
-        num = arr.index(problem) + 1
-        output(str(num) + "\n")
+        print(reverse_arr[problem])
 # 숫자를 입력해도 문자로 입력받기 때문에 type error 발생 => ASCII 코드를 이용해서 숫자 부분을 추출할까? => 숫자인지만 판별하려면 isdigit() or try 구문 사용하자!
 # 시간초과 발생! => 역시 리스트를 사용해서 시간초과가 발생한 걸까? => 번호를 알아야 하니 key 값을 숫자로 하여 풀어볼까?(dictonary)
+# 혹시 몰라서 빠른 입출력을 사용해 보았지만 시간초과 발생 => 역시 리스트 때문임에 틀림없다.
